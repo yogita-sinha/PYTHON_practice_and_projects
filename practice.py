b3ea7361bbc6288069aa11567d46903529b0841d
yogita-sinha <yogita.sinha2606@gmail.com>
2026-04-29 20:45:14 +0530

    Add files via upload

---
+++ practice.py
@@ -0,0 +1,13 @@
+marks = {
+
+
+}
+
+x = int(input("enter phy marks:"))
+marks.update({"phy":x})
+y = int(input("enter chem marks:"))
+marks.update({"chem":y})
+z = int(input("enter maths marks:"))
+marks.update({"maths":z})
+
+print (marks)
