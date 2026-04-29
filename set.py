38ad69848552b280e7325adc90aab7c11491bdff
yogita-sinha <yogita.sinha2606@gmail.com>
2026-04-29 20:45:40 +0530

    Add files via upload

---
+++ set.py
@@ -0,0 +1,10 @@
+#collection or set is a collection which is unordered,
+#unchangeable, and unindexed.
+#In Python, sets are defined using curly braces {} 
+#or the set() function.
+
+
+collection = {1 , 2, 3 , 4  , "hello" , 1.5, (1,2,3) }
+print (collection)
+print(type(collection))
+print(len(collection))
