2fff76d635e6bd534b4aa49526bcc17cb4fdbf2f
yogita-sinha <yogita.sinha2606@gmail.com>
2026-04-29 20:44:49 +0530

    Add files via upload

---
+++ pract1.py
@@ -0,0 +1,14 @@
+dictionary = {
+    "table" : ["a piece of furniture","lists of facts and figures"],
+    "cat" : "a small animal",
+}
+
+print(dictionary)
+print(dictionary["table"])  
+print(dictionary["cat"])
+
+subjects = {
+    "python" , "java" , "c++" , "python" , "javascript" , "java", "python","java", "c",
+}
+
+print(len(subjects)) 
