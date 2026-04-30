a515f6d997b8f843f1a249de3400c1b65963ba13
yogita-sinha <yogita.sinha2606@gmail.com>
2026-04-30 17:10:02 +0530

    Add files via upload

---
+++ loops.py
@@ -0,0 +1,23 @@
+# Loops in Python (while loop )
+
+# while loop syntax
+n = int(input("enter n :"))
+
+count  = 0
+
+
+while count <= n :
+    
+    count+=1
+
+    print(count)
+
+
+
+
+number = 13
+while  number>=1:
+
+    number -=1
+
+    print(number)
