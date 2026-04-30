677e98b0dcd4eacb0d64b2caafcd69c94481a726
yogita-sinha <yogita.sinha2606@gmail.com>
2026-04-30 18:01:09 +0530

    Add files via upload

---
+++ practiceQ.py
@@ -0,0 +1,20 @@
+#print num from 1 to 100
+
+for i in range (1 , 101):
+    print (i)
+
+print()
+print()
+
+#print num from 100 to 1
+for i in range (100 , 0 , -1):
+    print (i)
+
+print()
+print()
+
+#print table of n
+n = int(input("enter n :"))
+
+for i in range(11):
+    print(n*i)
