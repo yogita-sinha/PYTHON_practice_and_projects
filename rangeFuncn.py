073b1325a6b7265b1ba595a4851b0f8f94e57773
yogita-sinha <yogita.sinha2606@gmail.com>
2026-04-30 17:51:16 +0530

    Add files via upload

---
+++ rangeFuncn.py
@@ -0,0 +1,17 @@
+#range function 
+
+for i in range (11): #range(stop)
+    print(i)
+print()
+print()
+
+for i in range (1 , 11): #range(start , stop)
+    print(i)    
+
+print()
+print()
+
+for i in range (2 , 11 , 2): #range(start , stop , step) step-> kitne se increase hora hai
+    print(i) 
+print()
+print()           
