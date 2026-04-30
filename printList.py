692e59344378fa1ee477678bc65e1846e059e5ef
yogita-sinha <yogita.sinha2606@gmail.com>
2026-04-30 17:10:35 +0530

    Add files via upload

---
+++ printList.py
@@ -0,0 +1,18 @@
+list = [1 , 4 , 16 , 25 , 36 , 49 , 64 , 81 , 100 ]
+
+# print(list[0])
+# print(list[1])
+# print(list[2])
+# print(list[3])
+# print(list[4])
+# print(list[5])
+# print(list[6])  
+# print(list[7])
+# print(list[8])
+
+# same work print with the help of loop 
+
+idx = 0
+while idx < len(list):
+    print(list[idx])
+    idx +=1
