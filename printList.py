dde9a332d19b3d772d311c476c3a644fef41ca77
yogita-sinha <yogita.sinha2606@gmail.com>
2026-05-04 18:27:56 +0530

    Add files via upload

--- printList.py
+++ printList.py
@@ -1,18 +1,9 @@
-list = [1 , 4 , 16 , 25 , 36 , 49 , 64 , 81 , 100 ]
+def printList(list , idx ):
+    if (idx == len(list)):
+        return 
+    else :
+        print (list[idx])
+        printList(list , idx+1)
 
-# print(list[0])
-# print(list[1])
-# print(list[2])
-# print(list[3])
-# print(list[4])
-# print(list[5])
-# print(list[6])  
-# print(list[7])
-# print(list[8])
-
-# same work print with the help of loop 
-
-idx = 0
-while idx < len(list):
-    print(list[idx])
-    idx +=1
+fruits = ["apple", "banana", "grapes", "orange", "kiwi"]
+printList(fruits , 0) #index ki starting point 0 se hoti hai.       
