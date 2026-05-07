543d1580cb44780c9d11731068d2e956c207d468
yogita-sinha <yogita.sinha2606@gmail.com>
2026-05-07 22:33:09 +0530

    Add files via upload

---
+++ oop_constructor.py
@@ -0,0 +1,18 @@
+# creating class
+class student:
+
+    def __init__(self,fullname ):
+        # print(self)
+        self.name = fullname
+
+        print("addding new student in database")
+
+    
+
+#creating object (instance)
+s1 = student("muskan")
+# print(s1) internally self ka yahi matlb h.
+print(s1.name)
+
+s2 = student("stuti")
+print(s2.name)
