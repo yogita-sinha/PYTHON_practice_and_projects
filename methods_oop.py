19225ce2b477fda9ba7f6511e7102ffee063c56d
yogita-sinha <yogita.sinha2606@gmail.com>
2026-05-07 23:07:24 +0530

    Add files via upload

---
+++ methods_oop.py
@@ -0,0 +1,18 @@
+class Student:
+   college_name = "ABC College"
+   
+   def __init__(self, name, marks):
+    self.name = name
+    self.marks = marks
+    
+   def welcome(self) :
+      print ("welcome student",self.name)
+
+   def get_marks(self) :
+      return self.marks
+        
+
+s1 = Student("karan", 97)
+
+s1.welcome()
+print(s1.get_marks())
